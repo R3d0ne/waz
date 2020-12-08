b2dd569bd411270247b3aef502a02a6236e3d964
Radhouane Bel Hadj Yahia <45982196+R3d0ne@users.noreply.github.com>
2020-12-08 11:35:10 +0100

    Update server.sh

--- server.sh
+++ server.sh
@@ -5,12 +5,10 @@
 
 echo "--------------------------------------------------------------------------"
 echo "$(date)"
-echo "Starting Wazuh Made Easy"
 echo "Wazuh for Debian-based Systems"
 echo "Wazuh Manager - Wazuh API - Elasticsearch - Kibana"
 echo "-------------------------------------------------------------------------"
 
-echo "Wazuh Made Easy Status"
 
 echo " System Update..."
 
